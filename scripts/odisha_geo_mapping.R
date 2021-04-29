b98ef2679a3dfeed6a0cf9feec3799723e7229dd
Apoorv Anand <apoorv7491@gmail.com>
2021-04-29 22:35:08 +0530

    Match blocks

--- scripts/odisha_geo_mapping.R
+++ scripts/odisha_geo_mapping.R
@@ -19,7 +19,7 @@ geo_mapping <- geo_mapping[!is.na(geo_mapping$`SL NO. for AC`),]
 names(geo_mapping) <- c("g_ac_id","g_ac","g_block","g_gp","g_district","g_pc")
 geo_mapping <- geo_mapping %>% mutate_all(funs(str_replace_all(., "�", "")))
 geo_mapping <- geo_mapping %>% mutate_all(funs(str_trim(str_to_lower(.))))
-
+geo_mapping <- geo_mapping[!is.na(geo_mapping$g_block),]
 
 # District match
 
@@ -48,6 +48,42 @@ geo_mapping$updated_district_name[is.na(geo_mapping$updated_district_name)] <- g
 
 
 # Block match
+scheme_blocks <- unique(scheme_data[,c("s_district", "s_block")])
+geo_blocks <- unique(geo_mapping[,c("updated_district_name", "g_block")])
+odisha_blocks <- left_join(scheme_blocks, geo_blocks, by=c('s_district'='updated_district_name', 's_block'='g_block'),keep = T)
 
+
+# Fuzzy Match
+
+all_scheme_districts <- unique(odisha_blocks$s_district)
+odisha_block_match <- data.frame()
+block_original_rows <- 0
+for(i in 1:length(all_scheme_districts)){
+block_df <- odisha_blocks[odisha_blocks$s_district == all_scheme_districts[[i]],]  
+block_original_rows <- block_original_rows + nrow(block_df)
+geo_df <- geo_blocks[geo_blocks$updated_district_name == all_scheme_districts[[i]],]
+block_df <-
+  block_df %>% stringdist_left_join(geo_df[,"g_block"],
+                                         by = c('s_block' = 'g_block'),
+                                         max_dist = 1)
+names(block_df)[which(names(block_df)=='g_block.x')] <- 'block_direct_match'
+names(block_df)[which(names(block_df)=='g_block.y')] <- 'block_fuzzy_match'
+odisha_block_match <- bind_rows(odisha_block_match, block_df)
+}
+
+# Update blocks in the geography file
+odisha_blocks_to_update <- odisha_block_match[!is.na(odisha_block_match$block_fuzzy_match),]
+geo_mapping <-
+  left_join(
+    geo_mapping,
+    odisha_blocks_to_update[, c("s_district", "block_fuzzy_match", "s_block")],
+    by = c("updated_district_name" = "s_district", "g_block" = "block_fuzzy_match")
+  )
+names(geo_mapping)[which(names(geo_mapping)=='s_block')] <- 'updated_block_name'
 
 # GP match
+
+x <- unique(geo_mapping[,c("g_district", "g_block", "updated_district_name", "updated_block_name")])
+
+
+
