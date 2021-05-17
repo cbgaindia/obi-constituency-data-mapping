0276c648d1cd1c4656df2cf478674ee67c69a745
Apoorv Anand <apoorv7491@gmail.com>
2021-05-17 20:00:35 +0530

    Generate mapping summary for MNREGA 2018-19

--- scripts/generate_mapping_summary.R
+++ scripts/generate_mapping_summary.R
@@ -6,7 +6,8 @@ source("scripts/libraries.R")
 # Read Files --------------------------------------------------------------
 
 geo_mapping <- readr::read_csv(file = "data/geography/updated/geo-odisha-updated.csv")
-scheme_data <- readr::read_csv(file="data/scheme/MNREGA/odisha/2019-20/updated/odisha-mnrega-2019-updated.csv")
+# scheme_data <- readr::read_csv(file="data/scheme/MNREGA/odisha/2019-20/updated/odisha-mnrega-2019-updated.csv")
+scheme_data <- readr::read_csv(file="data/scheme/MNREGA/odisha/2018-19/updated/odisha-mnrega-2018-updated.csv")
 
 # Export mapping results --------------------------------------------------
 
@@ -67,4 +68,5 @@ state_mapping_summary <-
     "g_ac",
     "g_pc"
   )]
-readr::write_csv(state_mapping_summary, "data/results/scheme-geography-mapping-summary.csv")
+# readr::write_csv(state_mapping_summary, "data/results/mnrega-2019-20-geography-mapping-summary.csv")
+readr::write_csv(state_mapping_summary, "data/results/mnrega-2018-19-geography-mapping-summary.csv")
